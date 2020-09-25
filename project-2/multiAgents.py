78324173cbb2640a7e4a79a7246e91adf009baa9
Patrick Davlin <patrickdavlin@Patricks-iMac.localdomain>
2020-09-24 19:09:34 -0500

    carter updates

--- project-2/multiAgents.py
+++ project-2/multiAgents.py
@@ -14,7 +14,7 @@
 
 from util import manhattanDistance
 from game import Directions
-import random, util
+import random, util, sys
 
 from game import Agent
 
@@ -71,10 +71,27 @@ class ReflexAgent(Agent):
         newPos = successorGameState.getPacmanPosition()
         newFood = successorGameState.getFood()
         newGhostStates = successorGameState.getGhostStates()
+        newGhostPositions = successorGameState.getGhostPositions()
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
-        "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+        gds = [manhattanDistance(newPos, gpos) for gpos in newGhostPositions]
+        fps = [manhattanDistance(newPos, snack) for snack in newFood.asList()]
+        
+        #TODO: TELL PACMAN TO TAKE FOOD IF DISTANCE IS 0?
+        # print("food distances: ", fps)
+        if len(fps) == 0:
+            return 1000 #+ successorGameState.getScore()
+        else:
+            gs = min(gds)
+            fs = min(fps)
+            # print("mgd: ", gs, " mfd: ", fs) 
+            score = gs / (fs * 20)
+
+        if action.strip() == 'Stop':
+            score -= 100
+
+        score += successorGameState.getScore()
+        return score
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -135,7 +152,66 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        return self.max_value(gameState, 0)
+
+    def max_value(self, state, d):
+        # Return a score instead of action
+        # Returning STOP or something similar broke the agent
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+
+        # Get Pacman Actions
+        legalActions = state.getLegalActions(0)
+
+        # Lowest Possible score. Maybe there's a better way in Python to implement this
+        best_score = -1 * sys.maxsize
+        
+        # Test all actions for pacman
+        for a in legalActions:
+            next_state = state.generateSuccessor(0, a)
+            # Evaluate the ghost's scores beneath this max agent node
+            score = self.min_value(next_state, d, 1)
+            # Maintain the highest possible score for this node
+            if score > best_score:
+                best_score = score
+                best_action = a
+
+        if d > 0:
+            # Mid-depth max agent, return score up the tree
+            return best_score
+        else:
+            # Full d-depth tree explored, return action w/ best score
+            return best_action
+
+    def min_value(self, state, d, ghostNum):
+        # Return score if game over
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        # Get legal actions for this ghost
+        legalActions = state.getLegalActions(ghostNum)
+        # Return worst case score for ghost
+        best_score = sys.maxsize
+        for a in legalActions:
+            next_state = state.generateSuccessor(ghostNum, a)
+            # if this is the last ghost
+            if ghostNum == state.getNumAgents() - 1:
+                # if this is the deepest node in the search tree, return score
+                if d == (self.depth - 1):
+                    score = self.evaluationFunction(next_state)
+                # if middle depth, call another Max Agent
+                else:
+                    score = self.max_value(next_state, d+1)
+            # not last ghost, call next min agent
+            else:
+                score = self.min_value(next_state, d, ghostNum+1)
+
+            # Maintain min score
+            if score < best_score:
+                best_score = score
+
+        return best_score
+
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -146,8 +222,60 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """
         Returns the minimax action using self.depth and self.evaluationFunction
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.max_value(gameState, 0, -1 * sys.maxsize, sys.maxsize)
+
+    def max_value(self, state, d, alpha, beta):
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+
+        legalActions = state.getLegalActions(0)
+
+        best_score = -1 * sys.maxsize
+        
+        for a in legalActions:
+            next_state = state.generateSuccessor(0, a)
+            score = self.min_value(next_state, d, 1, alpha, beta)
+            if score > best_score:
+                best_score = score
+                best_action = a
+                if best_score > beta:
+                # print("best score >= passed beta")
+                    return best_score
+                if best_score > alpha:
+                    alpha = best_score
+            
+
+        if d > 0:
+            return best_score
+        else:
+            return best_action
+
+    def min_value(self, state, d, ghostNum, alpha, beta):
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+
+        legalActions = state.getLegalActions(ghostNum)
+        best_score = sys.maxsize
+
+        for a in legalActions:
+            next_state = state.generateSuccessor(ghostNum, a)
+            if ghostNum == state.getNumAgents() - 1:
+                if d == (self.depth - 1):
+                    score = self.evaluationFunction(next_state)
+                else:
+                    score = self.max_value(next_state, d+1, alpha, beta)
+            else:
+                score = self.min_value(next_state, d, ghostNum+1, alpha, beta)
+
+            if score < best_score:
+                best_score = score
+                if best_score < alpha:
+                    return best_score
+                if best_score < beta:
+                    beta = best_score
+            
+
+        return best_score
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -161,8 +289,65 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         All ghosts should be modeled as choosing uniformly at random from their
         legal moves.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.max_value(gameState, 0)
+
+    def max_value(self, state, d):
+        # Return a score instead of action
+        # Returning STOP or something similar broke the agent
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+
+        # Get Pacman Actions
+        legalActions = state.getLegalActions(0)
+
+        # Lowest Possible score. Maybe there's a better way in Python to implement this
+        best_score = -1 * sys.maxsize
+        
+        # Test all actions for pacman
+        for a in legalActions:
+            next_state = state.generateSuccessor(0, a)
+            # Evaluate the ghost's scores beneath this max agent node
+            score = self.ex_value(next_state, d, 1)
+            # Maintain the highest possible score for this node
+            if score > best_score:
+                best_score = score
+                best_action = a
+
+        if d > 0:
+            # Mid-depth max agent, return score up the tree
+            return best_score
+        else:
+            # Full d-depth tree explored, return action w/ best score
+            return best_action
+
+    def ex_value(self, state, d, ghostNum):
+        # Return score if game over
+        if state.isWin() or state.isLose():
+            return self.evaluationFunction(state)
+        # Get legal actions for this ghost
+        legalActions = state.getLegalActions(ghostNum)
+        # Return worst case score for ghost
+        running_score = 0
+        for a in legalActions:
+            p = 1 / len(legalActions)
+            next_state = state.generateSuccessor(ghostNum, a)
+            # if this is the last ghost
+            if ghostNum == state.getNumAgents() - 1:
+                # if this is the deepest node in the search tree, return score
+                if d == (self.depth - 1):
+                    score = self.evaluationFunction(next_state)
+                # if middle depth, call another Max Agent
+                else:
+                    score = self.max_value(next_state, d+1)
+            # not last ghost, call next min agent
+            else:
+                score = self.ex_value(next_state, d, ghostNum+1)
+
+            #maintain score:
+            running_score += (score * p)
+
+        # avg = running_score / (state.getNumAgents() - 1)
+        return running_score
 
 def betterEvaluationFunction(currentGameState):
     """
