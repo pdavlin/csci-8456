4f504dd91014bc1f74a0b0309066867789def4a4
Patrick Davlin <patrickdavlin@Patricks-iMac.local>
2020-11-17 13:28:15 -0600

    stuff runs

--- project-4/asprilo-encodings-develop/run.sh
+++ project-4/asprilo-encodings-develop/run.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# SYSTEM="clingo"
-SYSTEM="clingcon"
+SYSTEM="clingo"
+# SYSTEM="clingcon"
 
 # ENCODINGS="action-MPP-3.lp goal-D-r.lp output-MPPD-3.lp -c horizon=$2"
 # ENCODINGS="xapf/action-M.ilp   xapf/goal-m.ilp"
 # ENCODINGS="xapf/action-M-XY.ilp   xapf/goal-m-XY.ilp"
-ENCODINGS="xapf/action-M.iclp   xapf/goal-m.iclp"
-# VISUALIZER="--outf=0 -V0 --out-atomf=%s. | head -n1 | visualizer"
+# ENCODINGS="m/action-M.iclp   m/goal-m.iclp"
+ENCODINGS="."
+VISUALIZER="--outf=0 -V0 --out-atomf=%s. | head -n1 | visualizer"
 
-for f in $1/*.lp ;
-do
-    $SYSTEM $ENCODINGS $f;
-done
+clingo --out-atomf='%s.' -V0 \
+-c horizon=15 \
+$ENCODINGS/m/{action-M.lp,goal-M-mod.lp,output-M.lp} \
+../instances/x11_y6_n66_r1_s8_ps1_pr8_u8_o8_N001.lp
 
-# CALL="clingo1facts assign.lp $1 | clingo $ENCODINGS control.lp - $3 $VISUALIZER"
+# for f in $1/*.lp ;
+# do
+#     $SYSTEM $ENCODINGS $f;
+# done
+
+# # CALL="clingo1facts assign.lp $1 | clingo $ENCODINGS control.lp - $3 $VISUALIZER"
