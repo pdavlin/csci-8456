b72503f7475a613a4e0d6a7964c628196b7a61a3
Patrick Davlin <patrickdavlin@Patricks-iMac.local>
2020-11-17 15:14:50 -0600

    a bunch more stuff idk

--- project-4/asprilo-encodings-develop/run.sh
+++ project-4/asprilo-encodings-develop/run.sh
@@ -8,12 +8,17 @@ SYSTEM="clingo"
 # ENCODINGS="xapf/action-M-XY.ilp   xapf/goal-m-XY.ilp"
 # ENCODINGS="m/action-M.iclp   m/goal-m.iclp"
 ENCODINGS="."
-VISUALIZER="--outf=0 -V0 --out-atomf=%s. | head -n1 | visualizer"
+# VISUALIZER="--outf=0 -V0 --out-atomf=%s. | head -n1 | visualizer"
+VISUALIZER="head -n1 | visualizer"
 
-clingo --out-atomf='%s.' -V0 \
--c horizon=15 \
-$ENCODINGS/m/{action-M.lp,goal-M-mod.lp,output-M.lp} \
-../instances/x11_y6_n66_r1_s8_ps1_pr8_u8_o8_N001.lp
+# Part 1 Command
+# clingo --out-atomf='%s.' -V0 \
+# -c horizon=15 \
+# $ENCODINGS/m/{action-M.lp,goal-M.lp,output-M.lp} \
+# instances/x11_y6_n66_r1_s8_ps1_pr8_u8_o8_N001.lp > outfile
+
+clingo abc/{action-MPP.lp,goal-D-c.lp} examples/x4_y4_n16_r2_s3_ps1_pr2_u4_o2_N1.lp -c horizon=8
+output-MPPD.lp --outf=0 -V0 --out-atomf=%s. | head -n1 | viz
 
 # for f in $1/*.lp ;
 # do
