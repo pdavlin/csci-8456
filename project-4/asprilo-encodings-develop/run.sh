663003fb3d54a7226ca2744da51322c5f16a84e1
Patrick Davlin <patrickdavlin@Patricks-iMac.localdomain>
2020-11-18 21:36:28 -0600

    update for 4

--- project-4/asprilo-encodings-develop/run.sh
+++ project-4/asprilo-encodings-develop/run.sh
@@ -11,37 +11,30 @@ ENCODINGS="."
 # VISUALIZER="--outf=0 -V0 --out-atomf=%s. | head -n1 | visualizer"
 VISUALIZER="head -n1 | visualizer"
 
-# Exercise 1-1 Command
+# Exercise 1-1 
 # clingo --out-atomf='%s.' -V0 \
 # -c horizon=15 \
 # ./m/{action-M-mod.lp,goal-M-mod.lp,output-M.lp} \
 # instances/x11_y6_n66_r1_s8_ps1_pr8_u8_o8_N001.lp | viz
 
-# Exercise 1-2 Command
+# Exercise 1-2 
 # clingo --out-atomf='%s.' -V0 \
 # -c horizon=10 \
 # $ENCODINGS/m/{action-M-mod.lp,goal-M-mod.lp,output-M.lp} \
 # instances/x11_y6_n66_r1_s8_ps1_pr8_u8_o8_N001.lp | viz
 
-# Exercise 2-1 Command
-clingo --out-atomf='%s.' -V0 \
--c horizon=20 \
-$ENCODINGS/m/{action-M-mod.lp,goal-M.lp,output-M.lp} \
-instances/x11_y6_n66_r8_s8_ps1_pr8_u8_o8_N001.lp | viz
-
-# Exercise 2-2
+# Exercise 2 
 # clingo --out-atomf='%s.' -V0 \
-# -c horizon=25 \
-# $ENCODINGS/control/{sides.lp,highways.lp} \
-# $ENCODINGS/abc/{action-MPP.lp,goal-D-a.lp,output-MPPD.lp} \
-# instances/x9_y6_n54_r4_s8_ps2_pr8_u8_o8_N001.lp | viz
+# -c horizon=20 \
+# $ENCODINGS/m/{action-M-mod.lp,goal-M.lp,output-M.lp} \
+# instances/x11_y6_n66_r8_s8_ps1_pr8_u8_o8_N001.lp | viz
 
 # Exercise 3-1
 # clingo --out-atomf='%s.' -V0 \
 # -c horizon=25 \
 # $ENCODINGS/control/{sides.lp,highways.lp} \
 # $ENCODINGS/abc/{action-MPP.lp,goal-D-a.lp,output-MPPD.lp} \
-# instances/x9_y6_n54_r4_s8_ps2_pr8_u8_o8_N001.lp | viz
+# instances/x9_y6_n54_r4_s8_ps2_pr8_u8_o8_N001.lp > 3out2.txt
 
 # Exercise 3-2
 # clingo --out-atomf='%s.' -V0 \
@@ -49,15 +42,15 @@ instances/x11_y6_n66_r8_s8_ps1_pr8_u8_o8_N001.lp | viz
 # instances/x9_y6_n54_r4_s8_ps2_pr8_u8_o8_N001.lp | \
 # head -n 1 | \
 # clingo --out-atomf='%s.' -V0 \
-# -c horizon=40 \
+# -c horizon=60 \
 # - \
 # $ENCODINGS/control/{control-abc.lp,highways.lp} \
 # $ENCODINGS/abc/{action-MPP.lp,goal-D-a.lp,output-MPPD.lp} \
-# instances/x9_y6_n54_r4_s8_ps2_pr8_u8_o8_N001.lp | viz
+# instances/x9_y6_n54_r4_s8_ps2_pr8_u8_o8_N001.lp
 
 # Exercise 4
-# clingo --out-atomf='%s.' -V0 \
-# -c horizon=30 \
-# $ENCODINGS/control/{energy.lp,highways.lp} \
-# $ENCODINGS/abc/{action-MPP.lp,goal-D-a.lp,output-MPPD.lp} \
-# instances/x7_y6_n42_r3_s6_ps1_pr12_u24_o3_nrg_N001.lp
+clingo --out-atomf='%s.' -V0 \
+-c horizon=30 \
+$ENCODINGS/control/{energy.lp,highways.lp} \
+$ENCODINGS/abc/{action-MPP.lp,goal-D-a.lp,output-MPPD.lp} \
+instances/x7_y6_n42_r3_s6_ps1_pr12_u24_o3_nrg_N001.lp
